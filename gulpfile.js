0fdf90b9ea754aa3eddeedc97bbee8749a49d2cd
Gurza1992 <bakzbannyld@mail.ru>
2020-04-09 19:45:39 +0300

    Сетки и стили

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const server = require('browser-sync').create();
+const prefixer = require('gulp-autoprefixer');
+const {series} = require('gulp');
+const rename = require('gulp-rename');
+
+function Sass(){
+    return gulp.src(['scss/style.scss'],{base:'scss'})
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(prefixer())
+        .pipe(sourcemaps.write())
+        .pipe(rename("style.css"))
+        .pipe(gulp.dest('css'))
+        .pipe(server.stream())
+}
+
+
+function Server(){
+    server.init({server:"./"});
+    gulp.watch('scss/style.scss',gulp.series(Sass))
+}
+
+
+exports.start = series(Sass,Server);
+
+
